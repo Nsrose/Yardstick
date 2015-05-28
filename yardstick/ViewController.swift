f151289eeb1b39ff18830f38fe5b5da5faff97fc
nsrose <nsrose@berkeley.edu>
2015-05-27 21:27:31 -0700

    Initial Commit

---
+++ yardstick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yardstick
+//
+//  Created by Nick Rose on 5/27/15.
+//  Copyright (c) 2015 acmetools. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
